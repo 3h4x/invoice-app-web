ec5abcfd92bcc56e9ceb4bcdf314b83846048c27
Marcin Brański <6998565+3h4x@users.noreply.github.com>
2022-06-26 22:24:05 +0200

    Added mui theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
+import { blue } from '@mui/material/colors'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
 import type { AppProps } from 'next/app'
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+  },
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
