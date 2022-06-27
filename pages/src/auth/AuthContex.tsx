945006389699bed4124a4a08eab58e3f3a530d7b
Marcin Brański <6998565+3h4x@users.noreply.github.com>
2022-06-27 10:15:04 +0200

    Cookie auth

--- pages/src/auth/AuthContex.tsx
+++ pages/src/auth/AuthContex.tsx
@@ -1,9 +1,13 @@
-import { createContext, ReactNode, useContext, useState } from 'react'
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react'
+
+import { getCookie, setCookies } from 'cookies-next'
 
 type LoginParams = {
   email: string
   password: string
 }
+
+const AUTH_COOKIE_NAME = 'auth-token'
 
 export const AuthContext = createContext<null | {
   userToken: string | null
@@ -13,6 +17,18 @@ export const AuthContext = createContext<null | {
 
 export const AuthContextProvider = (props: { children: ReactNode }) => {
   const [userAuthToken, setAuthToken] = useState<string | null>(null)
+  const persistToken = (token: string) => {
+    setAuthToken(token)
+    setCookies(AUTH_COOKIE_NAME, token)
+  }
+
+  useEffect(() => {
+    const cookieToken = getCookie(AUTH_COOKIE_NAME)?.toString()
+    if (cookieToken) {
+      setAuthToken(cookieToken)
+    }
+  }, [])
+
   return (
     <AuthContext.Provider
       value={{
